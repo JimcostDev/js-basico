52f332f5625b2582dae895c8a0cc0fb2dd716a87
JimcostDev <rjak727@gmail.com>
2023-02-20 16:21:19 +0100

    que son las variables

--- 2-var.js
+++ 2-var.js
@@ -2,18 +2,20 @@
 Es una representación de algún lugar en la memoria para guardar datos
  */
 
-var nombre = "Diego"; // Nombre apuntará en algún lugar en la memoria (nosotros no sabemos donde, pero eso no es importante), pero dirá la señal de = tomará ese string y lo guardará en ese lugar.  y después si queremos utilizar ese valor, utilizaremos la variable Nombre para pedir que nos traigan ese string de memoria.
+// Nombre apuntará en algún lugar en la memoria (nosotros no sabemos donde, pero eso no es importante), pero dirá la señal de = tomará ese string y lo guardará en ese lugar.  y después si queremos utilizar ese valor, 
+// utilizaremos la variable Nombre para pedir que nos traigan ese string de memoria.
+var nombre = "Ronaldo"; 
 
 // = es igual a operador de asignación 
 
 var edad; // Declarar
-edad = 30; // Inicializar 
+edad = 23; // Inicializar 
 
 var elementos = ['Computadora', 'Celular']; // Aquí la variable seleccionará toda una sección para los elementos del array, y después tendremos acceso a toda la sección destinada en memoria.
 
 var persona = {   // de igual forma para el objeto
-    nombre: "Diego",
-    edad: 30
+    nombre: "Ronaldo",
+    edad: 23
 }
 
-console.log(elementos[2]); // consola es de igual forma una variable que le pertenece al navegador, con cierto espacio en memoria, y con un tipo de comportamiento especial. Donde Log es un método de la función
+console.log(elementos[1]); // consola es de igual forma una variable que le pertenece al navegador, con cierto espacio en memoria, y con un tipo de comportamiento especial. Donde Log es un método de la función
