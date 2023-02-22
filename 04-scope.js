0731cbd15f488cead263ff610fab9aefce920e31
JimcostDev <rjak727@gmail.com>
2023-02-22 18:54:20 +0100

    el scope de js - alcanze de las variables

--- 04-scope.js
+++ 04-scope.js
@@ -8,68 +8,55 @@
 // ===============
 
 // Ejemplo de lo que es un Scope Global
-
-var miNombre = "Diego"; 
-
+var miNombre = "Ronaldo"; 
 function nombre() {
-    miNombre = "Alonso";
     return miNombre;
 }
 
 nombre();
-
 console.log(miNombre);
 
+
 // Ejemplo de lo que es un Scope Local, el Scope local pasa al momento de crear una funcion, esto genera un ámbito diferente al global, al cual no se podrá tener acceso desde el ámbito global 
-
-var miNombre = "Diego"
-
+var miNombre = "Ronaldo"
 function nombre() {
-    var miApellido = "De Granda"; 
+    var miApellido = "Jimenez"; 
     return miNombre + " " + miApellido 
 }
 
-nombre();
+console.log(nombre());
+console.log(miApellido); // error
 
-console.log(miNombre);
-console.log(miApellido);
 
-//  Ejemplos de como puede ayuar o crear error el tema del scope. 
-
-// Ejemplo 1, cómo con una funcion podemos vambiar el valor de la variable global
+// Ejemplo 1, cómo con una funcion podemos cambiar el valor de la variable global
 var miNombre = "Diego"
-
 function nombre(usuario) {
     miNombre = usuario;
     console.log(miNombre); 
 }
 
 nombre("Oscar");
-
 console.log(`Hola ${miNombre}, cómo estás?`);
 
 
 // Ejemplo 2, cómo podemos evitar reescribir el valor de una variable gracias al scope,
 var miNombre = "Diego"
-
 function nombre(usuario) {
     var miNombre = usuario;
     console.log(miNombre); 
 }
 
 nombre("Oscar");
-
 console.log(`Hola ${miNombre}, cómo estás?`);
 
-// Ejemplo 3, cómo si creamos un scope local, y mandamos llamar la variable fuera de la funcion, nos puede crear un problema. 
 
+// Ejemplo 3, cómo si creamos un scope local, y mandamos llamar la variable fuera de la funcion, nos puede crear un problema. 
 function nombre(usuario) {
     var miNombre = usuario;
     console.log(miNombre); 
 }
 
 nombre("Oscar");
-
 console.log(`Hola ${miNombre}, cómo estás?`);
 
 
