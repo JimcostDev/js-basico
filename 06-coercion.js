343ba8a0c5ba81b93298c0015e266cb634ea57aa
JimcostDev <rjak727@gmail.com>
2023-02-23 15:28:20 +0100

    coercion - la forma de convertir un type a otro

--- 06-coercion.js
+++ 06-coercion.js
@@ -1,26 +1,21 @@
 // La forma de convertir un type a otro: Coercion 
 
 // === Implicit Coercion === 
-
 var a = 20;
-
 var b = a + ""; 
+typeof b; // string
 
 // más ejemplos
-
 var a = [1,2];
 var b = [3,4]; 
-
-a + b // En este ejemplo, por la concatenación, ambos arrays son obligados a convertirse en strings, y luego se juntan.
+var c = a + b // En este ejemplo, por la concatenación, ambos arrays son obligados a convertirse en strings, y luego se juntan.
+typeof c; // string
 
 // === Explicit coercion ===
-
 var c = String( a );
-
 var d = Number( c );
 
 // Otro ejemplo
-
 var a = 30; 
 var b = a.toString();
 
@@ -28,7 +23,6 @@ var c = "100";
 var d = +c; // Unary operator '+' de forma explícita convierte a número
 
 // más ejemplos
-
 var num1 = "3.14"; 
 var num2 = 5 + +num1; 
 
