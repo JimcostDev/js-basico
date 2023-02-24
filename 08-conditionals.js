fe9ed0751efc98aadec06a1a91e3d7ca68e17fcc
JimcostDev <rjak727@gmail.com>
2023-02-24 14:05:47 +0100

    condicionales

--- 08-conditionals.js
+++ 08-conditionals.js
@@ -1,15 +1,11 @@
 // Esto nos ayudará para poder hacer decisiones.
-
 var esUsuario = true; 
-
 if(esUsuario) {  // con el if validamos que la condición sea true
     console.log('Tiene acceso al contenido'); 
 }
 
 // Presentamos el else 
-
 var esUsuario = false; 
-
 if(esUsuario) {
     console.log('Tiene acceso al contenido'); 
 } else {
@@ -17,7 +13,6 @@ if(esUsuario) {
 }
 
 // Ejemplo de cómo haríamos una validación
-
 var edad = 18; 
 var accion; 
 
@@ -26,22 +21,19 @@ if(edad >= 18) {
 } else { // call back 
     accion = 'No puede votar'
 }
-
 console.log(accion);
 
 // Ejemplo de cómo utilizaremos el else if
-
 var edad = 18; 
 var accion;  
 
 if(edad === 18) {
     accion = 'Puede votar, será su 1ra votación'
 } else if(edad > 18) { 
-    accion = 'Puede votar'
+    accion = 'Puede votar de nuevo'
 } else { // call back 
     accion = 'Aun no puede votar'
 }
-
 console.log(accion);
 
 
@@ -54,29 +46,13 @@ var numero = 1;
 var resultado; 
 
 if(numero === 1) {
-    resultado = 'Sí son un 1'; 
+    resultado = 'Sí soy un 1'; 
 } else {
     resultado = 'No soy un 1'; 
 }
 
-condition ? true : false; 
+// condition ? true : false; 
 
 var numero = 1; 
-var resultado = numero === 1 ? 'Sí son un 1' : 'No soy un 1';
-
+var resultado = numero === 1 ? 'Sí soy un 1' : 'No soy un 1';
 console.log(resultado);
-
-
-// ==============================
-
-
-// Ejercicio, reaizar un piedra, papel o tijeras, utilizando condicionales, una funcion que reciba el parametro con el que vamos a jugar, y me regrese si gane o perdí. 
-
-/*  
-
-Variables piedra, papel y tiejeras 
-una funcion que utilice cindicionales para validar si el parametro que le estoy pasando gana o pierde. 
-
-y que la funcion al final me regrese el resultado de si gané, o  perdí. 
-
-*/ 
