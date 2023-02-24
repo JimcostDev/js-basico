aedfb7efaf0890210b69f3507c23fe06d8ea060b
JimcostDev <rjak727@gmail.com>
2023-02-24 15:39:28 +0100

    switch

--- 10-switch.js
+++ 10-switch.js
@@ -1,19 +1,19 @@
 // Switch es una forma diferente de validar, está depende del caso que sea verdad. 
 
-var numero = 1; 
-var resultado; 
+var numero = 1, 
+    resultado; 
 
 /* Switch compara con un === por esa razón los elementos tiene que ser idénticos */ 
 
 switch (numero) {
     case 1: 
-        resultado = "Sí es uno";
+        resultado = "Caso 1";
         break;
     case 10: 
-        resultado = "Sí es diez";
+        resultado = "Caso 10";
         break;
     case 100:
-        resultado = "Sí es cien";
+        resultado = "Caso 100";
         break;
     default:   
         resultado = "No match"; 
