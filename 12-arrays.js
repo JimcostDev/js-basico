a95258987d62f0bee010561a55484ae36f4d50c0
JimcostDev <rjak727@gmail.com>
2023-02-28 15:04:45 +0100

    arrays add metodos

--- 12-arrays.js
+++ 12-arrays.js
@@ -35,3 +35,18 @@ console.log(frutas);
 var posicion = frutas.indexOf("Platano"); // te dará la posición de ese item en el array
 console.log(frutas);
 
+
+/*El método “map()” permite aplicar una función a cada elemento 
+del array y devolver un nuevo array con los valores modificados. Por ejemplo:*/
+const numeros = [1, 2, 3, 4, 5];
+const cuadrados = numeros.map(function(numero) {
+  return numero * numero;
+});
+console.log(cuadrados); // [1, 4, 9, 16, 25]
+
+
+// El método “reduce()” permite combinar todos los elementos del array en un solo valor. Por ejemplo:
+const suma = numeros.reduce(function(acumulador, numero) {
+    return acumulador + numero;
+  }, 0);
+  console.log(suma); // 15
